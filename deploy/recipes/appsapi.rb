9b66cfb7429f0273bddbdb7109d042b36c0da0bb
Kit Ho <kithokit@mangowish.com>
2015-01-30 15:01:23 +0800

    add script

--- deploy/recipes/appsapi.rb
+++ deploy/recipes/appsapi.rb
@@ -2,26 +2,26 @@ include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
 
-  if deploy[:application_type] != 'other'
-    Chef::Log.debug("Skipping deploy::appsapi for application #{application} as it is not a other app")
-    next
-  end
+  #if deploy[:application_type] != 'other'
+    #Chef::Log.debug("Skipping deploy::appsapi for application #{application} as it is not a other app")
+    #next
+  #end
 
   case deploy[:database][:type]
   when "mysql"
-    connector_jar = node['opsworks_java']['tomcat']['mysql_connector_jar']
-    connector_jar_path = ::File.join(node['opsworks_java']['tomcat']['java_shared_lib_dir'], connector_jar)
-    include_recipe "opsworks_java::mysql_connector"
+    connector_jar = node['opsworks_appsapi']['tomcat']['mysql_connector_jar']
+    connector_jar_path = ::File.join(node['opsworks_appsapi']['tomcat']['java_shared_lib_dir'], connector_jar)
+    include_recipe "opsworks_appsapi::mysql_connector"
   when "postgresql"
     connector_jar = node[:platform].eql?('ubuntu') ? 'postgresql-jdbc4.jar' : 'postgresql-jdbc.jar'
-    connector_jar_path = ::File.join(node['opsworks_java']['tomcat']['java_shared_lib_dir'], connector_jar)
-    include_recipe "opsworks_java::postgresql_connector"
+    connector_jar_path = ::File.join(node['opsworks_appsapi']['tomcat']['java_shared_lib_dir'], connector_jar)
+    include_recipe "opsworks_appsapi::postgresql_connector"
   else
     connector_jar = ""
     connector_jar_path = ""
   end
 
-  link ::File.join(node['opsworks_java']['tomcat']['lib_dir'], connector_jar) do
+  link ::File.join(node['opsworks_appsapi']['tomcat']['lib_dir'], connector_jar) do
     to connector_jar_path
     action :create
     only_if { ::File.file?(connector_jar_path) }
@@ -46,12 +46,12 @@ node[:deploy].each do |application, deploy|
   end
 
   current_dir = ::File.join(deploy[:deploy_to], 'current')
-  webapp_dir = ::File.join(node['opsworks_java'][node['opsworks_java']['java_app_server']]['webapps_base_dir'], webapp_name)
+  webapp_dir = ::File.join(node['opsworks_appsapi'][node['opsworks_appsapi']['java_app_server']]['webapps_base_dir'], webapp_name)
 
   # opsworks_deploy creates some stub dirs, which are not needed for typical webapps
   ruby_block "remove unnecessary directory entries in #{current_dir}" do
     block do
-      node['opsworks_java'][node['opsworks_java']['java_app_server']]['webapps_dir_entries_to_delete'].each do |dir_entry|
+      node['opsworks_appsapi'][node['opsworks_appsapi']['java_app_server']]['webapps_dir_entries_to_delete'].each do |dir_entry|
         ::FileUtils.rm_rf(::File.join(current_dir, dir_entry), :secure => true)
       end
     end
@@ -62,14 +62,14 @@ node[:deploy].each do |application, deploy|
     action :create
   end
 
-  include_recipe "opsworks_java::#{node['opsworks_java']['java_app_server']}_service"
+  include_recipe "opsworks_appsapi::#{node['opsworks_appsapi']['java_app_server']}_service"
 
-  execute "trigger #{node['opsworks_java']['java_app_server']} service restart" do
+  execute "trigger #{node['opsworks_appsapi']['java_app_server']} service restart" do
     command '/bin/true'
-    not_if { node['opsworks_java'][node['opsworks_java']['java_app_server']]['auto_deploy'].to_s == 'true' }
-    notifies :restart, "service[#{node['opsworks_java']['java_app_server']}]"
+    not_if { node['opsworks_appsapi'][node['opsworks_appsapi']['java_app_server']]['auto_deploy'].to_s == 'true' }
+    notifies :restart, "service[#{node['opsworks_appsapi']['java_app_server']}]"
   end
 end
 
-include_recipe 'opsworks_java::web_app'
-include_recipe 'opsworks_java::context'
+include_recipe 'opsworks_appsapi::web_app'
+include_recipe 'opsworks_appsapi::context'
